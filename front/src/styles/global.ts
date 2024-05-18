ec0f32144e68d50466bc0b43e89464258cd5cf7c
Andre Luiz <adventureandre@hotmail.com>
2024-05-18 17:07:50 -0300

    Configurando Theme e Eslint Fixes #14

---
+++ front/src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0;
+  background-color: red;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+`
